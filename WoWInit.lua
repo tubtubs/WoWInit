ed23f6a5651571be0a27eff962c32496a17543da
Tubtubs <96892771+tubtubs@users.noreply.github.com>
2026-02-09 05:36:03 -0700

    Added Dewdrop Context Menu
    Also renovated the form a bit. Should be very close to centered,  with better fitting text box.

--- WoWInit.lua
+++ WoWInit.lua
@@ -2,14 +2,15 @@
 ** WoWInit **
 by Tubtubs
 Setup commands to run after you login to WoW. Type /wi, or click the minimap to access the editor.
+
 Includes presets for:
 pfQuest
 morphHelper
-
 ]]--
 local libIcon = LibStub("LibDBIcon-1.0");
 local libData = LibStub("LibDataBroker-1.1");
-
+local WI_Dewdrop = AceLibrary("Dewdrop-2.0");
+local WI_DewdropSubMenu = AceLibrary("Dewdrop-2.0");
 
 --init functions
 function WI_Run()
@@ -21,6 +22,72 @@ function WI_Run()
         DEFAULT_CHAT_FRAME:AddMessage(WI_Vars.Commands)
         WI_ExecuteCMDs()
     end
+    WI_MinimapIconRegister()
+    WI_DewdropRegister()
+    DEFAULT_CHAT_FRAME:AddMessage(WI_SPLASH)
+end
+
+function WI_ExecuteCMDs()
+	for w in string.gfind(WI_Vars.Commands, "([^\r\n]+)") do
+        DEFAULT_CHAT_FRAME.editBox:SetText(w)
+        ChatEdit_SendText(DEFAULT_CHAT_FRAME.editBox, 0)
+	end
+end
+
+--Dropdown Menu Code
+function WI_PasteExample(txt)
+    w = WI_CMDFrame_ScrollFrame_EditBox:GetText()
+    WI_CMDFrame_ScrollFrame_EditBox:SetText(w .. txt)
+end
+
+function WI_ToggleExampleMenu()
+    if WI_Dewdrop:IsOpen() then
+        WI_Dewdrop:Close();
+    else
+        WI_Dewdrop:Open(this);
+    end
+end
+
+function WI_DewdropRegister()
+	WI_Dewdrop:Register(WI_CMDFrame_ExamplesButton, --Bound Frame
+		'point', function(parent) --Point
+			return "TOP", "BOTTOM"
+		end,
+		'children', function(level, value) --Children
+			if level == 1 then
+                for i,j in ipairs(WI_EXAMPLES) do
+                    if j.check() then
+                        WI_Dewdrop:AddLine(
+                            'text', j.name,
+                            'tooltipTitle', j.name,
+                            'tooltipText', j.tooltip,  
+                            'textR', 1,
+                            'textG', 0.82,
+                            'textB', 0,
+                            'func', WI_PasteExample,
+                            'arg1', j.example,
+                            'notCheckable', true
+                        )
+                    end
+                end
+
+				--Close button
+				WI_Dewdrop:AddLine(
+					'text', "Close Menu",
+					'textR', 0,
+					'textG', 1,
+					'textB', 1,
+					'func', function() WI_Dewdrop:Close() end,
+					'notCheckable', true
+				)
+			end
+		end,
+		'dontHook', true
+	)
+end
+
+--Minimap Button Setup
+function WI_MinimapIconRegister()
     local iconData = libData:NewDataObject("WoWInit icon data", {
         OnClick = function()
             if WI_CMDFrame:IsShown() then
@@ -38,24 +105,34 @@ function WI_Run()
     });
 
     libIcon:Register("WoWInit icon", iconData, WoWInit_Icon);
-    DEFAULT_CHAT_FRAME:AddMessage(WI_SPLASH)
-end
-
-function WI_ExecuteCMDs()
-	for w in string.gfind(WI_Vars.Commands, "([^\r\n]+)") do
-        DEFAULT_CHAT_FRAME.editBox:SetText(w)
-        ChatEdit_SendText(DEFAULT_CHAT_FRAME.editBox, 0)
-	end
 end
 
 --Command Entry Frame
 function WI_CMDFrame_SubmitButton_OnClick()
     WI_Vars.Commands = WI_CMDFrame_ScrollFrame_EditBox:GetText()
-    WI_CMDFrame:Hide();
+    WI_CMDFrame:Hide()
 end
 
 function WI_CMDFrame_OnShow()
     WI_CMDFrame_ScrollFrame_EditBox:SetText(WI_Vars.Commands)
+    WI_CMDFrame_ScrollFrame_EditBox:SetFocus()
+    local max
+    _, max =  WI_CMDFrame_ScrollFrameScrollBar:GetMinMaxValues();
+    WI_CMDFrame_ScrollFrameScrollBar:SetValue(max);
+    WI_CMDFrame_SubmitButton_Update()
+end
+
+function WI_CMDFrame_OnHide()
+    WI_Dewdrop:Close(1)
+end
+
+function WI_CMDFrame_SubmitButton_Update()
+    w = WI_CMDFrame_ScrollFrame_EditBox:GetText()
+    if w == "" or w==WI_Vars.Commands then
+        WI_CMDFrame_SubmitButton:Disable()
+    else
+        WI_CMDFrame_SubmitButton:Enable()
+    end
 end
 
 --chat commands
--- localisations.lua
+++ localisations.lua
@@ -4,5 +4,57 @@ WI_VERSION = "1.0"
 WI_FULLNAME = WI_ADDONNAME .. " v" .. WI_VERSION .. " by " .. WI_AUTHOR
 WI_SPLASH = WI_FULLNAME .. " loaded." 
 
-WI_CMDFrameTitle = WI_ADDONNAME.." CMDs"
-WI_SUBMITBUTTON = "Submit"
+WI_CMDFrameTitle = WI_ADDONNAME
+WI_SUBMITBUTTON = "Save"
+WI_EXAMPLESBUTTON = "Examples"
+WI_CANCELBUTTON = "Cancel"
+WI_HELPTEXT = "Enter the macro you'd like to run on startup below.\nClick Examples to paste in some common macros."
+
+WI_EXAMPLES = 
+-- name     - title of the button, and tooltip
+-- tooltip  - the text that displays on the tooltip, under the tooltip title
+-- example  - the command to paste into the command box. Usually prepended with newline
+-- check    - returns if the example should be included. 
+--          Ex: returns false if the relevant addon isn't installed, or just returns true
+{
+    {
+        name = "Console Print",
+        tooltip = "Prints Test to the chat frame",
+        example = "\n/run DEFAULT_CHAT_FRAME:AddMessage(\"Test\")",
+        check = function() 
+            return true
+        end,
+    },
+    {
+        name = "MorphHelper",
+        tooltip = "Morphs the player into a murloc",
+        example = "\n/mh morph player 31",
+        check = function() 
+            if (MH_Vars) then
+                return true
+            else
+                return false
+            end
+        end,
+    },
+    {
+        name = "pfQuest",
+        tooltip = "Tracks chests, rares and closes the map if it opens",
+        example = "\n/db track chests\n/db track rares\n/run if WorldMapFrame:IsShown() then ToggleWorldMap() end",
+        check = function() 
+            if (pfQuest) then
+                return true
+            else
+                return false
+            end
+        end,
+    },
+    {
+        name = "Print Rested",
+        tooltip = "Prints rested XP % in chat",
+        example = "\n/script local r = GetXPExhaustion() if r then DEFAULT_CHAT_FRAME:AddMessage(\"Rested XP: \" .. tostring(math.floor((r / UnitXPMax(\"player\")) * 100)) .. \"%\") else DEFAULT_CHAT_FRAME:AddMessage(\"No Rested XP.\") end",
+        check = function() 
+            return true
+        end,
+    },
+}
